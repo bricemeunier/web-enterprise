2aa73252ae61c3b962d1da917011081086e483ed
brice meunier <brice.meunier@etu.unilim.fr>
2020-04-22 22:47:45 +0200

    issue fixed

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,12 +1,15 @@
 <?php
+
 define('DBHOST', "us-cdbr-iron-east-01.cleardb.net");
 define('DBNAME', "heroku_533ee06e10e9fe5");
 define('DBUSER', "b0cc52e050ec32");
 define('DBPASSWD', "b75e251b");
+
 /*
 define('DBHOST', "localhost");
-define('DBNAME', "webEnterprise");
+define('DBNAME', "webenterprise");
 define('DBUSER', "root");
-define('DBPASSWD', "");*/
+define('DBPASSWD', "");
 define('ENV','dev'); //prod for development environment
+*/
 ?>
